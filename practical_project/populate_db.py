33865785f20ffcf2440de7b809c369d7121dc69d
2466402m <2466402m@student.gla.ac.uk>
2024-03-21 13:20:46 +0000

    styling

--- practical_project/populate_db.py
+++ practical_project/populate_db.py
@@ -9,27 +9,29 @@ from pets_for_students.models import Student, Cat
 def populate():
     students_data = [
         {'first_name': 'Alyssa', 'last_name': 'Croft', 'cats': ['Alex', 'Luna', 'Mittens']},
-        {'first_name': 'Jone', 'last_name': 'Doe', 'cats': ['Muffins']},
-        {'first_name': 'Aza,', 'last_name': 'Khan', 'cats': ['Jill', 'Joe']}, 
+        {'first_name': 'John', 'last_name': 'Doe', 'cats': ['Muffins']},
+        {'first_name': 'Azam', 'last_name': 'Khan', 'cats': ['Jill', 'Joe']}, 
     ]
 
     for student_data in students_data:
-        student, created = Student.objects.get_or_create(
-            first_name=student_data['first_name'],
-            last_name=student_data['last_name']
-        )
-
-        # Update number of cats for the student
-        student.cat_num = len(student_data['cats'])
-        student.save()
-
-        # Add cats for the student
+        student = add_student(student_data['first_name'], student_data['last_name'])
         for cat_name in student_data['cats']:
-            cat, created = Cat.objects.get_or_create(
-                name=cat_name,
-                owner=student
-            )
+            add_cat(student, cat_name)
 
+    for student in Student.objects.all():
+        student.cat_num = student.cat_set.count()
+        student.save()
+        # print(f'Student: {student.full_name}, Number of cats: {student.cat_num}')
+        # for cat in student.cat_set.all():
+        #     print(f'- Cat: {cat.name}')
+
+def add_student(first_name, last_name):
+    student = Student.objects.get_or_create(first_name=first_name, last_name=last_name)[0]
+    return student
+
+def add_cat(student, name):
+    cat = Cat.objects.get_or_create(owner=student, name=name)[0]
+    return cat
 if __name__ == '__main__':
     print('Starting population script...')
     populate()
